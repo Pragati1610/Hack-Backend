41e4993e83737b2ff660e50c83b885d82ab51c78
V1NT4G3C0D3 <pragatibhattad1610@gmail.com>
2020-10-22 15:30:17 +0530

    add all review details

--- routes/review.js
+++ routes/review.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const jwtAuth = require('../middlewares/jwtAuthMiddleware');
 const adminAuth = require('../middlewares/adminAuthMiddleware');
 
-router.get('/allReviews/:reviewId', [jwtAuth, adminAuth], async (req, res) => {
-  const response = await events.getAllReview(req.params.reviewId);
+router.get('/allReviews/:eventId', [jwtAuth, adminAuth], async (req, res) => {
+  const response = await review.getAllReview(req.params.eventId);
   return res.status(response.isError ? 400 : 200).send(response);
 });
 
