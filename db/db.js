875bc2300e4c4b105abf268b97176008408115e4
pragati1610 <pragatibhattad1610@gmail.com>
2020-12-03 02:11:30 +0530

    remove unnecessary sql log

--- db/db.js
+++ db/db.js
@@ -1,17 +1,18 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize({
-  database: process.env.DB_DATABASE,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  host: process.env.DB_HOST,
-  port: 5432,
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  }
+    database: process.env.DB_DATABASE,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    port: 5432,
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    },
+    logging: false
 });
 module.exports = sequelize;
