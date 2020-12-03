db94df3f86baf179f3ff376f7a9df6756601275c
pragati1610 <pragatibhattad1610@gmail.com>
2020-12-04 00:44:30 +0530

    change datatype for abstract from string to text

--- models/team.js
+++ models/team.js
@@ -6,7 +6,7 @@ const schema = {
     teamId: { type: DataTypes.UUID, defaultValue: Sequelize.UUIDV4, primaryKey: true },
     teamCode: { type: DataTypes.STRING },
     teamName: { type: DataTypes.STRING, allowNull: false },
-    abstract: { type: DataTypes.STRING },
+    abstract: { type: DataTypes.TEXT },
     link: { type: DataTypes.STRING } // title for ideathon
 };
 
