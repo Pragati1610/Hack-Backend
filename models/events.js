6e1742d0de1b74cd7942ac79b06182d097270988
pragati1610 <pragatibhattad1610@gmail.com>
2021-04-12 18:01:25 +0530

    fix: change date format

--- models/events.js
+++ models/events.js
@@ -1,12 +1,25 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = require('../db/db');
+const moment = require("moment");
 
 const schema = {
     eventId: { type: DataTypes.UUID, defaultValue: Sequelize.UUIDV4, primaryKey: true },
     eventName: { type: DataTypes.STRING, allowNull: false },
     problemStatements: { type: DataTypes.ARRAY(DataTypes.STRING), allowNull: false },
-    dateOfEvent: { type: DataTypes.DATE, allowNull: false },
-    endOfEvent: { type: DataTypes.DATE, allowNull: false },
+    dateOfEvent: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        get: function() {
+            return moment(this.getDataValue('DateTime')).format('DD.MM.YYYY')
+        }
+    },
+    endOfEvent: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        get: function() {
+            return moment(this.getDataValue('DateTime')).format('DD.MM.YYYY')
+        }
+    },
     maxTeamSize: { type: DataTypes.INTEGER, allowNull: false }
 };
 
--- routes/sendMailsToQualified.js
+++ routes/sendMailsToQualified.js
@@ -1,28 +1,23 @@
 const xlsxFile = require('read-excel-file/node');
 
-const sendClearedRoundMail = require('./templates/sendClearedRoundMail');
 const mail = require('../routes/mail');
-const disqualified = require('./templates/disqualified');
-const wt = require('./templates/wt2020');
-const cert = require('./templates/ideathonCert');
-const club = require('./templates/clubs');
-const reminder = require('./templates/reminder');
+const reminder = require('./reminder');
 
 xlsxFile('./data.xlsx').then((rows) => {
 
     for (let i = 0; i <= rows.length - 2; i++) {
         let emails = [rows[i + 1][1]];
-        let details = { name: rows[i + 1][0] };
+        // let details = { name: rows[i + 1][0] };
         let htmlPart = {
             Charset: "UTF-8",
-            Data: reminder(details)
+            Data: reminder()
         };
         let subject = {
             Charset: 'UTF-8',
-            Data: `
+            Data: `WomenTechies'21 Participation Certificates
             `
         }
         mail(emails, htmlPart, subject);
-        // console.log(details);
+        console.log(emails);
     }
 })
